b6d9f684eb055c2854a7213a6e91acc9507326cf
Prashanth Pai <ppai@redhat.com>
2014-07-14 16:02:20 +0530

    Update 20.sh

--- 20.sh
+++ 20.sh
@@ -2,11 +2,15 @@
 # http://lifehacker.com/5591835/reduce-computer+caused-eye-strain-with-the-20+20+20-rule
 # gnome-screensaver package is required
 
+# Run this as normal user and not root!
+# gnome-session-properties can be used to trigger this script after login.
+
 while :
 do
         sleep 20m
-        if (gnome-screensaver-command -q | grep -q "not"); then
-                notify-send -t 1000 "20-20-20" "Look away for 20 seconds"
+        # Note: The string we are doing a grep on might have changed between releases!
+        if (gnome-screensaver-command -q | grep -q "inactive"); then
+                notify-send --hint=int:transient:1 "Hey!" "Look away for 20 seconds"
         fi
         sleep 20s
 done
